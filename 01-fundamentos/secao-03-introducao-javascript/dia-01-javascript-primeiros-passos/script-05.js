12dc48782ab30336c5954e2aebb795f872f0a36c
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-23 15:53:31 -0300

    FIXANDO APRENDIZADO JS-0

---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/script-05.js
@@ -0,0 +1,11 @@
+// const conditionOne = true;
+// const conditionTwo = false;
+// console.log(conditionOne && conditionTwo);
+
+const cenouras = true;
+const leite = true;
+const arroz = true;
+const feijao = true;
+const listaDeCompras = cenouras && leite && arroz && feijao;
+
+console.log(listaDeCompras)
