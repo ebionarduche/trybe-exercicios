01a72e4b0be4b048f6e40f7f3e48d1d4a646fb17
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-23 19:33:12 -0300

    PRIMEIRO EXERCICIO DO DIA

---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio-01/script.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 2;
+ 
+let adicao = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+console.log(divisao);
