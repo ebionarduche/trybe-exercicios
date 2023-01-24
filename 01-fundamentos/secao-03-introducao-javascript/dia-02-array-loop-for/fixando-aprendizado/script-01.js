a7349a59088a391c5bdc847c25f42ac302585541
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-24 13:54:54 -0300

    add pasta do dia-02

---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-02-array-loop-for/fixando-aprendizado/script-01.js
@@ -0,0 +1,12 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Tomar banho');
+tasksList.unshift('Preparar o café');
+
+
+let searchFirstTask = tasksList[0];
+console.log(searchFirstTask);
+
+let searchLastTask = tasksList[tasksList.length - 1];
+console.log(searchLastTask);
+
