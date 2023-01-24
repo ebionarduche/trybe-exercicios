758776e1f3593ac92abca3b4e36be68fe49241df
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-24 15:05:26 -0300

    fixando aprendizado

--- 01-fundamentos/secao-03-introducao-javascript/dia-02-array-loop-for/fixando-aprendizado/script-01.js
+++ 01-fundamentos/secao-03-introducao-javascript/dia-02-array-loop-for/fixando-aprendizado/script-01.js
@@ -1,12 +1,23 @@
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
-tasksList.push('Tomar banho');
-tasksList.unshift('Preparar o café');
+// tasksList.push('Tomar banho');
+// tasksList.unshift('Preparar o café');
 
 
-let searchFirstTask = tasksList[0];
-console.log(searchFirstTask);
+// let searchFirstTask = tasksList[0];
+// console.log(searchFirstTask);
 
-let searchLastTask = tasksList[tasksList.length - 1];
-console.log(searchLastTask);
+// let searchLastTask = tasksList[tasksList.length - 1];
+// console.log(searchLastTask);
 
+
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+ 
+// console.log(menuServices = menu[1]);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato');
+console.log(menu);
---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-02-array-loop-for/fixando-aprendizado/script-02.js
@@ -0,0 +1,5 @@
+var numero = 7;
+
+for (let contador = 1; contador <= 9; contador += 1){
+    console.log(numero * contador);
+}
