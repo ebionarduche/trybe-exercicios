d6b309f56f5327fe7fed381c59bfa1bd1bc94195
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-24 16:21:28 -0300

    exercicios do dia

---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-02-array-loop-for/fixando-aprendizado/script.05.js
@@ -0,0 +1,13 @@
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+ 
+// for (let index of names) {
+//     console.log(index);
+// }
+for(let index = 0; index < 10; index+=1 ){
+    console.log(index);
+  }
+  
+  for(let index = 0; index <= 20; index+=1 ){
+    console.log(index);
+  }
+  
