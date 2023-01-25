7a3316c7eaa899c4ea076ced60e43b35274f5175
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-24 22:10:09 -0300

    exercicios treino

--- 01-fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/treino/exercicio08.js
+++ 01-fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/treino/exercicio08.js
@@ -1,4 +1,10 @@
-const cutoDoProduto = 10;
+const custoDoProduto = 10;
 const ValorDeVenda = 20;
 
-if ()
+if (custoDoProduto >= 0 && ValorDeVenda >= 0) {
+    const custoTotal = custoDoProduto * 1.20;
+    const lucroTotal = (ValorDeVenda - custoTotal) * 1000;
+    console.log('O lucro total é de: R$' + lucroTotal + ",00");
+} else {
+    console.log('Error 404 - valores não podem ser negativos')
+}
