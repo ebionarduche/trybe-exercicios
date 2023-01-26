abb90bfedbe7b7b2be3551e7a5f326d4385e3967
Ebio Narduche <ebionarduche77@gmail.com>
2023-01-26 18:28:07 -0300

    exercicios em aula

---
+++ 01-fundamentos/secao-03-introducao-javascript/dia-04-objetos-funcoes/fixando-aprendizado/fixando-aprendizado03.js
@@ -0,0 +1,11 @@
+// function bomDiaTryber(nome) {
+//     return 'bom dia'
+// }
+
+// bomDiaTryber('Ebio')
+
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(property + ' = ' + object[property]);
+}
