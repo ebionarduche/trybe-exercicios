822169f85ec53a641a8a81ab3e1dc1997b4eca8c
Ebio Narduche <ebionarduche77@gmail.com>
2023-03-08 06:24:17 -0300

    exercicios reduce()

---
+++ 01-fundamentos/secao-07-higher-order-functions-js-es6/dia-03-higher-order-functions-filter-reduce/exercicios/countries.js
@@ -0,0 +1,86 @@
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
+
+// 1 - Calcule a quantidade total da população de todos os países.
+
+const populationCalculator = () => countries.reduce((acc, cur) => acc + cur.population,0);
+// console.log(populationCalculator())
+
+// 2 - Calcule a área total de todos os países.
+
+const totalAreaCalculator = () => countries.reduce((acc, cur) => acc + cur.area,0);
+// console.log(totalAreaCalculator())
+
+//🚀 3 - Encontre o país com o maior nome.
+
+const higherNameCountry = () => countries.reduce((acc, cur) => 
+  cur.name.length > acc.name.length ? cur : acc);
+  console.log(higherNameCountry())
+
+  //🚀 4 - Retorne a quantidade de vezes que a letra a maiúscula ou minúscula aparece no array de nomes.
+
