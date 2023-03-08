be9915b744ab9c4af510e87fd61c8ea8b276f175
Ebio Narduche <ebionarduche77@gmail.com>
2023-03-08 07:19:58 -0300

    exercicios reduce() final part

--- 01-fundamentos/secao-07-higher-order-functions-js-es6/dia-03-higher-order-functions-filter-reduce/exercicios/countries.js
+++ 01-fundamentos/secao-07-higher-order-functions-js-es6/dia-03-higher-order-functions-filter-reduce/exercicios/countries.js
@@ -94,4 +94,19 @@ const letterArary = names.join('').split('');
 const ContLetter = () => letterArary.reduce((acc, cur) =>
 cur === 'a' || cur === 'A' ? acc += 1 : acc, 0);
 
-console.log(ContLetter())
+// console.log(ContLetter())
+
+//🚀 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const nameAndAvarage = students.map((studant, index) => {
+  return {
+    students: studant,
+    Avarage: grades[index].reduce((acc, cur) => acc + cur,0) / grades[index].length
+  }
+})
+
+console.log(nameAndAvarage)
