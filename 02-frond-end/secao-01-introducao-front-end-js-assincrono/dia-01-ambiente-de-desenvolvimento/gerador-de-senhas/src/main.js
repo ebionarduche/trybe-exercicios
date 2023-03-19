0ad7ac47905101646e5820c83e4571820104fdc1
Ebio Narduche <ebionarduche77@gmail.com>
2023-03-19 11:25:35 -0300

    dia-01-front-end-part-02

--- 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import { nanoid } from "nanoid";
+
+const passwordBt = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2')
+console.log(passwordBt);
+
+passwordBt.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
