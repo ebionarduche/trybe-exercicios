57ed013bcfbf4cfcf474bbd986ecc5ac462d0536
Ebio Narduche <ebionarduche77@gmail.com>
2023-03-27 17:28:49 -0300

    praticando códigos assincronos

--- 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/exercicio-fixacao/index.js
+++ 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/exercicio-fixacao/index.js
@@ -11,10 +11,31 @@ import {
 const usersSelect = document.querySelector('#users-select');
 
 const USERS_API = 'https://dummyjson.com/users';
+
+fetch(USERS_API)
+    .then((response) => response.json())
+    .then((data) => {
+        const { users } = data;
+        fillUsersSelect(users);
+        // test(users);
+
+    });
+
+// const test = (users) => {
+//     users.forEach(({ age }) => console.log(age));
+// };
+
 // faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
 
 usersSelect.addEventListener('change', () => {
     clearPageData();
 
-    // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
+    const POSTS_API = `https://dummyjson.com/posts/user/${usersSelect.value}`;
+
+    fetch(POSTS_API)
+        .then((response) => response.json())
+        .then((data) => {
+            const { posts } = data;
+            fillPosts(posts);
+        });
 });
