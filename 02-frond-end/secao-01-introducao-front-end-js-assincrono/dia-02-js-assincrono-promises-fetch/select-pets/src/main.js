1268fe47c1fae1d15c9e782491497f9112a04751
Ebio Narduche <ebionarduche77@gmail.com>
2023-03-28 16:24:56 -0300

    fixando aprendizado react

--- 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/select-pets/src/main.js
+++ 02-frond-end/secao-01-introducao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/select-pets/src/main.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+
+const btnDog = document.querySelector('#btn-dog')
+const btnCAt = document.querySelector('#btn-cat')
+const btnSuprise = document.querySelector('#btn-suprise')
+
+
+const BASE_URl_DOG = 'https://dog.ceo/api/breeds/image/random'
+const BASE_URl_CAT = 'https://aws.random.cat/meow'
