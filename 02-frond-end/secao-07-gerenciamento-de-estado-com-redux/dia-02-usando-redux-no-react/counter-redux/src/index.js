08c086999a730d239f60f012c95d48cf87b9700b
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-08 09:35:56 -0300

    exercise counter with redux

--- 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-redux/src/index.js
+++ 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-redux/src/index.js
@@ -1,15 +1,13 @@
+// ./src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
 import store from './redux';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <App />
   </Provider>,
 );
-
