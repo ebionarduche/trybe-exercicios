e79cfce010a2705c86e9ed2e95d8abdc3e2e2287
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-10 17:05:51 -0300

    game of thunks

---
+++ 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-actions-assincronas/game-of-thunks/src/redux/reducers/CharacterReducer.jsx
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoading: true,
+  character: '',
+}
+
+const characterReducer = (state = initialState, action) => {
+ switch (action.type){
+  default:
+    return state}
+};
+
+export default characterReducer
