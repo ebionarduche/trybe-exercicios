3fa9927b7c19bd368bf250cc9d2c771c4bb41f09
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-10 17:43:18 -0300

    game of thunks

--- 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-actions-assincronas/game-of-thunks/src/redux/reducers/CharacterReducer.jsx
+++ 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-actions-assincronas/game-of-thunks/src/redux/reducers/CharacterReducer.jsx
@@ -1,10 +1,29 @@
+import { SEARCH_BEGIN, SEARCH_ERROR, SEARCH_SUCCESS } from "../actions";
+
 const initialState = {
-  isLoading: true,
+  isLoading: false,
   character: '',
 }
 
 const characterReducer = (state = initialState, action) => {
  switch (action.type){
+  case SEARCH_BEGIN:
+    return {
+      ...state,
+      isLoading: true,
+    }
+  case SEARCH_SUCCESS:
+    return {
+      ...state,
+      isLoading: false,
+      character: action.character
+    }
+  case SEARCH_ERROR:
+    return {
+      ...state,
+      isLoading: false,
+      error: action.error,
+    }
   default:
     return state}
 };
