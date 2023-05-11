defdf561a617f51b9f4fa693e3379668ca2d91f0
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-11 15:16:55 -0300

    counter test with redux

--- 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-actions-assincronas/game-of-thunks/src/redux/index.jsx
+++ 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-actions-assincronas/game-of-thunks/src/redux/index.jsx
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly";
+import { composeWithDevTools } from "@redux-devtools/extension"
 import thunk from "redux-thunk";
 import characterReducer from "./reducers/CharacterReducer";
 
