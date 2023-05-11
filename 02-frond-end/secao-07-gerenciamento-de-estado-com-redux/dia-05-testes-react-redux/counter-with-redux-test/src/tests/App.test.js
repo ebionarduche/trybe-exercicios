db43baf1f2bff41fc65e361e205163d8e832581e
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-11 13:36:03 -0300

    counter test with redux

--- 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-05-testes-react-redux/counter-with-redux-test/src/tests/App.test.js
+++ 02-frond-end/secao-07-gerenciamento-de-estado-com-redux/dia-05-testes-react-redux/counter-with-redux-test/src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore, combineReducers } from "redux";
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import App from '../App'
 import counterReducer from "../redux/reducers/counterReducer";
 
@@ -23,3 +24,21 @@ test('A página deve renderzar dois botões e o número "0"', () => {
   expect(buttonAdicionar.length).toBe(2);
   expect(screen.getByText('0')).toBeInTheDocument();
 });
+
+test('testa o click dos botões', () => {
+  renderWithRedux(<App />);
+
+  expect(screen.getByText('0')).toBeInTheDocument();
+
+
+  const buttons = screen.queryAllByRole('button')
+
+  userEvent.click(buttons[0]);
+  expect(screen.getByText(/1/i)).toBeInTheDocument();
+
+  userEvent.click(buttons[1]);
+  expect(screen.getByText(/5/i)).toBeInTheDocument();
+
+  
+  
+})
