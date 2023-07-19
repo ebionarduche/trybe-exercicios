e68b5b8903fcc8fc0077e25ff4b822a20f8c45b0
Ebio Narduche <ebionarduche77@gmail.com>
2023-07-19 08:53:19 -0300

    Começando exercicios dia 04

--- 03-back-end/secao-04-introducao-dev-node-js/dia-04-node-js-express-middlewares/exercicios/src/app.js
+++ 03-back-end/secao-04-introducao-dev-node-js/dia-04-node-js-express-middlewares/exercicios/src/app.js
@@ -1,5 +1,12 @@
-const express = require('express')
+const express = require('express');
+const validadeName = require('../middlewares/validadeName');
 
 const app = express()
+
+app.use(express.json());
+
+app.post('/activities', validadeName, (_req, res) => {
+  res.status(201).json({message: 'Atividade registrada com sucesso!'})
+})
 
 module.exports = app;
